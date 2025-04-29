7dbf5628ab5a6c4bab907cb2955edab117cf3d2e
Alejandro Durando <ale.durando@outlook.com>
2025-04-28 21:54:08 -0300

    detalles y correcciones finales, ademas del apartado 4

--- tpl1-tp4.c
+++ tpl1-tp4.c
@@ -6,7 +6,7 @@
 
 typedef struct {
     int TareaID; //Numérico autoincremental comenzando en 1000
-    char *Descripcion; //
+    char *Descripcion; //char menor a 100 caracteres.
     int Duracion; //entre 10 – 100
 }Tarea;
 
@@ -22,82 +22,103 @@ void insertarNodo(Nodo **start, Nodo *NNodo);
 Nodo * BuscarNodo(Nodo ** Start,int id);
 Nodo * QuitarNodo(Nodo ** Start,int id);
 void ListarLista(Nodo **start);
+void BuscarPalabraClave(Nodo *L1, Nodo *L2);
 
 int main(){
     int valor;
     int indice=1000,iaux,opcion;
 
-   
     Nodo * start = CrearListaVacia();
     Nodo * startLR= CrearListaVacia();
     Nodo * aux;
 
-    printf("Ingresar opcion:");
-    scanf("%d",&opcion);
-    switch (opcion)
-    {
-        case 1: {
-                    do{
-                        printf("Lista de tares pendientes: \n");
-                        printf("¿Desea cargar una tarea?( [1].SI [0].NO ) : ");
-                        scanf("%d",&valor);
+    do{
+        
+        printf("\n [1]Agregar Lista de tareas pendientes: ");
+        printf("\n [2]Marcar Tarea pendiente como realizada: ");
+        printf("\n [3]Mostrar Listas pendientes y realizadas: ");
+        printf("\n [4]Buscar por id o palabra clave en lista: ");
+        printf("\n [5]Salir: ");
+        printf("\n\t\t Ingresar opcion: ");
+        scanf("%d",&opcion);
+        
+        switch (opcion)
+        {
+            case 1: {
+                        do{
+                            
+                            printf("¿Desea cargar una tarea pendiente?( [1].SI [0].NO ) : ");
+                            scanf("%d",&valor);
 
-                        if(valor==1)insertarNodo(&start,crearNodoTarea(indice));                  
-                    
-                    }while(valor!=0);
+                            if(valor==1){
+                                    
+                                    insertarNodo(&start,crearNodoTarea(indice));
+                                    indice++;
+                                    }                  
+                        
+                        }while(valor!=0);
 
+                
+                        }
             
+            break;
+        
+            case 2:{
+                        printf("\n ID de la tarea pendiente para marcar realizada(0 para terminar): ");
+                        scanf("%d",&iaux);
+                        
+                        while(iaux!=0){
+                            aux=BuscarNodo(&start,iaux);
+                            
+                                if(aux){ //Si existe el nodo que devuelve buscarnodo.
+                                    aux=QuitarNodo(&start,iaux); //lo quito al nodo de la lista pendientes
+                                    insertarNodo(&startLR,aux); //agrego al nodo a la lista realizados.
+                                    
+                                }else {
+                                    printf("id no encontrado");
+                                    
+                                };
+                
+                        printf("\n ID de la tarea pendiente para marcar realizada(0 para terminar):  ");
+                        scanf("%d",&iaux);
+                
+                        }
                     }
-        
-        break;
-    
-    default:
-        break;
-    }
-    
-        printf("\n ID de la tarea pendiente para marcar realizada(0 para terminar): ");
-        scanf("%d",&iaux);
-        
-        
-        while(iaux!=0){
 
-     
-        aux=BuscarNodo(&start,iaux);
+            break;
             
-                if(aux){
-                    aux=QuitarNodo(&start,iaux);
-                    insertarNodo(&startLR,aux);
+            case 3: {    
+                    printf("\n\n LISTA DE TARES REALIZADAS: \n");
+                    ListarLista(&startLR);
+                    printf("\n\n LISTA DE TAREAS PENDIENTES: \n");
+                    ListarLista(&start);
+                    }
+            break;
+            
+            case 4:{ 
                     
-                }else {
-                    printf("id no encontrado");
-                    
-                };
+                    BuscarPalabraClave(start,startLR);
 
-        printf("\n ID de la tarea pendiente para marcar realizada(0 para terminar):  ");
-        scanf("%d",&iaux);
-
+            }
+            break;
+            
+            
+            default:
+            break;
         }
-
-
     
-    
-    printf("\n\n LISTA DE TARES REALIZADAS\n");
-    ListarLista(&startLR);
-    printf("\n\n LISTA DE TAREAS PENDIENTES\n");
-    ListarLista(&start);
-
-
-
+    }while(opcion!=5);
 
     return 0;
 
 }
 
+
+
 void * crearNodoTarea(int indice){
 
     Nodo *NNodo=(Nodo *)malloc(sizeof(Nodo));
     
-    indice++;
     int tareaDuracion,control=0;
     char *buff = (char *)malloc(sizeof(char)*100); //creamos variable buffer. 
     
@@ -148,7 +169,6 @@ Nodo * BuscarNodo(Nodo ** Start,int id)
   return Aux;
 }
 
-
 Nodo * CrearListaVacia()
 {
     return NULL;
@@ -184,15 +204,127 @@ Nodo * QuitarNodo(Nodo ** Start,int id)
     
   }
 
-  void ListarLista(Nodo **start){
+void ListarLista(Nodo **start){
 
     Nodo *aux;
     aux=*start;
     
     while(aux){
         printf("Indice: %d \n",aux->T.TareaID);
+        printf("Descricion: ");
+        puts(aux->T.Descripcion);
         printf("Duracion: %d \n",aux->T.Duracion);
         aux=aux->Sig;
     }
 
   }
+
+
+void BuscarPalabraClave(Nodo *LPendientes, Nodo *LRealizadas){
+    int opcion;
+    char *clave;
+
+    printf("\n Buscar por: \n[1].Palabra Clave \n[2].ID \n Seleccionar opcion: ");
+    scanf("%d",&opcion);
+    getchar();
+    fflush(stdin);
+    
+
+    switch (opcion)
+    {
+    case 1: //caso de busqueda con palabra clave. 
+        {
+            char *buff = (char *)malloc(sizeof(char)*100); //creamos un buffer auxiliar para alojar el string hasta 100 caracteres.
+            printf("\nIngrese la palabra clave a buscar: ");
+            fflush(stdin);
+            gets(buff);
+            getchar();
+            
+            clave = (char *)malloc((strlen(buff)+1)*sizeof(char)); //palabra para comparar y ya con memoria justa para la palabra.
+            strcpy(clave, buff); //copiando buffer en clave.
+            free(buff); //liberamos el buffer.
+
+            while(LPendientes!=NULL && strstr(LPendientes->T.Descripcion,clave)==NULL){
+                LPendientes=LPendientes->Sig; //recorremos hasta encontrar la palabra.
+            }
+            if(LPendientes!=NULL){
+                printf("\n Palabra clave encontrada: ");
+                printf("\n Se encuentra en Listado Tarea Pendientes: ");
+                printf("\n Id de tarea: %d",LPendientes->T.TareaID);
+                printf("\n Descripcion de la tarea: ");
+                puts(LPendientes->T.Descripcion);
+                printf("\n Duracion de la tarea: %d",LPendientes->T.Duracion);
+
+                    // muestro el nodo encontrado.
+            }else{
+                while (LRealizadas!=NULL && strstr(LRealizadas->T.Descripcion,clave)==NULL){
+                    LRealizadas=LRealizadas->Sig;
+                }
+                if(LRealizadas!=NULL){
+                    
+                    printf("\n Palabra clave encontrada: ");
+                    printf("\n Se encuentra en Listado Tarea Realizadas: ");
+                    printf("\n Id de tarea: %d",LRealizadas->T.TareaID);
+                    printf("\n Descripcion de la tarea: ");
+                    puts(LRealizadas->T.Descripcion);
+                    printf("\n Duracion de la tarea: %d",LRealizadas->T.Duracion);
+                    //muestro nodo.
+                }
+
+            }
+            free(clave); //liberamos la palabra usada.
+        }
+        break;
+    
+
+    case 2:{ //caso de busqueda con id.
+
+
+            //declaro un id auxiliar, un nodo auxiliar ya que voy a usar la funcion Buscarnodo que devuelve el nodo que encuentra.
+            int idbuscado;
+            Nodo *aux=(Nodo*)malloc(sizeof(Nodo));
+            printf("\nIngresar ID a buscar: ");
+            scanf("%d",&idbuscado);
+            fflush(stdin);
+            getchar();
+
+
+            aux=BuscarNodo(&LPendientes,idbuscado);
+            if(aux!=NULL){ //si lo encuentra en la lista de pendientes lo muestra y si no, prueba en el siguiente else.
+                printf("\n ID encontrado: ");
+                printf("\n Se encuentra en Listado Tarea Pendientes: ");
+                printf("\n Id de tarea: %d",LPendientes->T.TareaID);
+                printf("\n Descripcion de la tarea: ");
+                puts(LPendientes->T.Descripcion);
+                printf("\n Duracion de la tarea: %d",LPendientes->T.Duracion);
+            }else {
+                aux=BuscarNodo(&LRealizadas,idbuscado);
+                if(aux!=NULL){
+                    printf("\n ID encontrado: ");
+                    printf("\n Se encuentra en Listado Tarea Realizadas: ");
+                    printf("\n Id de tarea: %d",LRealizadas->T.TareaID);
+                    printf("\n Descripcion de la tarea: ");
+                    puts(LRealizadas->T.Descripcion);
+                    printf("\n Duracion de la tarea: %d",LRealizadas->T.Duracion);
+                    
+                }else {
+
+                    printf("\n ID no encontrado en ninguna lista.");
+                }
+    
+            }
+            free (aux);
+        }
+        
+
+            break;
+
+
+
+    default:
+        break;
+    }
+
+    
+
+}
